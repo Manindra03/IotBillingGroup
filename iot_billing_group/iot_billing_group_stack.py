afe8e3cd5eaa7b553366eef8b4cb27a6b2748f6c
Manindra <manindra03@gmail.com>
2024-11-30 18:01:44 +0530

    Added Iotbilling group

---
+++ iot_billing_group/iot_billing_group_stack.py
@@ -0,0 +1,22 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_iot as iot,Tags,
+)
+from constructs import Construct
+
+class IotBillingGroupStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Create an IoT billing group
+        billing_group = iot.CfnBillingGroup(self, "RSTBillingGroup",
+            billing_group_name="RST-billing-group",
+            billing_group_properties=iot.CfnBillingGroup.BillingGroupPropertiesProperty(
+                billing_group_description="RST billing group for IoT"
+            )
+        )
+        # Add tags to the billing group
+        Tags.of(billing_group).add("Appname", "RST")
+        Tags.of(billing_group).add("AppId", "38080")
